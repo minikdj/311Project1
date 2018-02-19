e8b11676b3e08710513ef035e7e9f15a9d6763ea
minikdj <minikdj11@gmail.com>
2018-02-19 09:51:16 -0500

    added comments, took out initialize_game()

--- TTTGame.py
+++ TTTGame.py
@@ -3,21 +3,18 @@ from tkinter import *
 
 #list used to keep the state of the game
 game_arr = [[0 for x in range(3)] for y in range(3)]
+turn = 'x'
+game_over = False
 
-def console_game():
+#loop for console view
+def console_view():
+	#while(game_over == False):
 	for x in range(3):
 		for y in range(3):
-			game_arr[x][y] = 'x'
 			print(game_arr[x][y], end='')
 		print('\n')	
-	print('---')
-	game_arr[1][2] = 'o'
-	for x in range(3):
-		for y in range(3):
-			print(game_arr[x][y], end='')
-		print('\n')
-
-
+	#need to ask for the user input
+	
 #loop to use the gui
 root = Tk()
 
@@ -45,11 +42,16 @@ def draw_o(x,y):
 	c.create_oval(x-50,y-50,x+50,y+50)
 
 def callback(event):
+	global turn
 	c.focus_set()
 	x = get_x_quadrant(event.x)
 	y = get_y_quadrant(event.y)
-	draw_o(x,y)
-	draw_x(x,y)
+	if(turn == 'x'):
+		draw_x(x,y)
+		turn = 'o'
+	else:
+		draw_o(x,y)
+		turn = 'x'
 
 
 frame = Frame(root, width=300, height=300)
@@ -65,6 +67,7 @@ c.create_line(200,1,200,300)
 c.bind("<Button-1>", callback)
 frame.pack()
 
+
 #if the command line argument is 1, use the gui
 #if the command line argument is not 1, use the console view
 if (sys.argv[1] == '1'):
@@ -72,4 +75,4 @@ if (sys.argv[1] == '1'):
 	root.mainloop()
 else:
 	#using the console view 
-	console_game()
+	console_view()
